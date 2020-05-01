cb97a3b24e7b3be905ea89566e864813a8025755
Michael Brooks <mike@snp.com>
2020-05-01 10:29:38 -0400

    Initial commit for php7.3 branch

--- init_container.sh
+++ init_container.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
+cat >/etc/motd <<EOL
+  _____
+  /  _  \ __________ _________   ____
+ /  /_\  \\___   /  |  \_  __ \_/ __ \
+/    |    \/    /|  |  /|  | \/\  ___/
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/
+A P P   S E R V I C E   O N   L I N U X
 
-# set -e
+Documentation: http://aka.ms/webapp-linux
+PHP quickstart: https://aka.ms/php-qs
 
-php -v
-# setup server root
-test ! -d "$HOME_SITE" && echo "INFO: $HOME_SITE not found. creating..." && mkdir -p "$HOME_SITE"
-if [ ! $WEBSITES_ENABLE_APP_SERVICE_STORAGE ]; then
-    echo "INFO: NOT in Azure, chown for "$HOME_SITE
-    chown -R www-data:www-data $HOME_SITE
-fi
+EOL
+cat /etc/motd
 
-# setup nginx log dir
-# http://nginx.org/en/docs/ngx_core_module.html#error_log
-# sed -i "s|error_log /var/log/error.log;|error_log stderr;|g" /etc/nginx/nginx.conf
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
 
-echo "INFO: creating /run/php/php7.0-fpm.sock ..."
-test -e /run/php/php7.0-fpm.sock && rm -f /run/php/php7.0-fpm.sock
-mkdir -p /run/php
-touch /run/php/php7.0-fpm.sock
-chown www-data:www-data /run/php/php7.0-fpm.sock
-chmod 777 /run/php/php7.0-fpm.sock
-echo "Starting SSH ..."
 service ssh start
-
-echo "Starting php-fpm ..."
-service php7.0-fpm start
-chmod 777 /run/php/php7.0-fpm.sock
-
-echo "Starting Nginx ..."
-mkdir -p /home/LogFiles/nginx
-if test ! -e /home/LogFiles/nginx/error.log; then
-    touch /home/LogFiles/nginx/error.log
-fi
-/usr/sbin/nginx
+sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
+mkdir /var/lock/apache2
+mkdir /var/run/apache2
+/usr/sbin/apache2ctl -D FOREGROUND
